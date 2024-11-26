584703aaac8c1cffc4430cebe9dae5cdcc9e70a1
AprilQx <xxq_010223@163.com>
2024-11-26 16:30:27 +0000

    fixed the bugs in the model architecture, getting parameters from toml file

--- mnist_addition/models/Mnist_Addition_nn.py
+++ mnist_addition/models/Mnist_Addition_nn.py
@@ -3,16 +3,17 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from typing import Dict, Any
 
 #First version
-class Mnist_Addition_nn(nn.Module):
+class MNISTAdditionNN(nn.Module):
     def  _init_(self, hidden_size=128,num_layers=2,dropout_rate=0.2):
         super().__init__()
         layers=[nn.Flatten(),
                 nn.linear(1568,hidden_size),
                 nn.ReLU(),
                 nn.Dropout(dropout_rate)]
-        for i in range(num_layers-1):
+        for _ in range(num_layers-1):
             layers.extend([
                 nn.Linear(hidden_size, hidden_size),
                 nn.ReLU(),
@@ -22,5 +23,19 @@ class Mnist_Addition_nn(nn.Module):
         layers.append(nn.Linear(hidden_size, 19))
         self.network=nn.Sequential(*layers)
         self.embedding=layers[-2]
+
     def forward(self, x):
         return self.network(x)
+    
+    def get_embeddings(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.network[:-1]:
+            x = layer(x)
+        return x
+
+def create_model(config: Dict[str, Any]) -> MNISTAdditionNN:
+    """Create a new MNIST Addition model from config."""
+    return MNISTAdditionNN(
+        hidden_size=config['model']['hidden_size'],
+        num_layers=config['model']['num_layers'],
+        dropout_rate=config['model']['dropout_rate']
+    )
