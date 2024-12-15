6cf53847d8b408c7abc3ee02804591547b120565
AprilQx <xxq_010223@163.com>
2024-12-15 19:23:36 +0000

    changed the position of  plots of visualizing tuning

--- mnist_addition/utils/visualize_tuning.py
+++ mnist_addition/utils/visualize_tuning.py
@@ -114,11 +114,10 @@ class TuningVisualizer:
             print(f"- {param}: [{self.df[param].min()}, {self.df[param].max()}]")
 
 def main():
-    PROJECT_ROOT = Path(__file__).resolve().parent.parent
-    sys.path.append(str(PROJECT_ROOT))
-    print(f"Current working directory: {os.getcwd()}")
-    results_path = "experiments/optuna_20241204_164643/all_results.json"
-    save_dir = Path("experiments/optuna_20241204_164643/visualizations")
+    project_root = Path(__file__).resolve().parent.parent.parent
+  
+    results_path = project_root/"experiments/optuna_20241204_164643/all_results.json"
+    save_dir = Path(project_root/"experiments"/"weight visualizations")
     
     # Create save directory if it doesn't exist
     save_dir.mkdir(exist_ok=True,parents=True)
